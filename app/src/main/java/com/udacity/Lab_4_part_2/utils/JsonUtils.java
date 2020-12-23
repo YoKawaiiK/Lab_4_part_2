e2cd5d28e67de762d876ab90c648d53a5d810649
YoKawaiiK <danil.legioner@gmail.com>
2020-12-23 16:53:02 +0700

    Парсинг json и десериализация данных массива в объект класса Sandwich

--- app/src/main/java/com/udacity/Lab_4_part_2/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/Lab_4_part_2/utils/JsonUtils.java
@@ -2,9 +2,77 @@ package com.udacity.Lab_4_part_2.utils;
 
 import com.udacity.Lab_4_part_2.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    // Ключи объекта Sandwich
+    public static final String KEY_NAME = "name";
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOW_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_INGREDIENTS = "ingredients";
+    // Пустое значение
+    public static final String VALUE_EMPTY = "Unknown";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+
+            JSONObject sandwichJson = new JSONObject(json);
+
+            JSONObject name = sandwichJson.getJSONObject(KEY_NAME);
+            String mainName = name.getString(KEY_MAIN_NAME);
+            JSONArray jsonAlsoKnownAs = name.getJSONArray(KEY_ALSO_KNOW_AS);
+            List<String> alsoKnownAs = new ArrayList<>(jsonAlsoKnownAs.length());
+            for (int i = 0; i < jsonAlsoKnownAs.length(); i++) {
+                alsoKnownAs.add(jsonAlsoKnownAs.getString(i));
+            }
+
+            if (alsoKnownAs.isEmpty()) alsoKnownAs.add(VALUE_EMPTY);
+
+            String placeOfOrigin = sandwichJson.getString(KEY_PLACE_OF_ORIGIN);
+
+            if (placeOfOrigin.isEmpty()) {
+                placeOfOrigin = VALUE_EMPTY;
+            }
+
+            String description = sandwichJson.getString(KEY_DESCRIPTION);
+
+            if (description.isEmpty()) {
+                description = VALUE_EMPTY;
+            }
+
+            String image = sandwichJson.getString(KEY_IMAGE);
+
+            JSONArray jsonIngredients = sandwichJson.getJSONArray(KEY_INGREDIENTS);
+            List<String> ingredients = new ArrayList<>(jsonIngredients.length());
+            for (int i = 0; i < jsonIngredients.length(); i++) {
+                ingredients.add(jsonIngredients.getString(i));
+            }
+
+            if (ingredients.isEmpty()) ingredients.add(VALUE_EMPTY);
+
+            // Создаем новый объект sandwich
+            Sandwich sandwich = new Sandwich(
+                    mainName,
+                    alsoKnownAs,
+                    placeOfOrigin,
+                    description,
+                    image,
+                    ingredients
+            );
+
+            return sandwich;
+        }
+        catch (Throwable error) {
+            return null;
+        }
     }
 }
