7768a160146bf021eaff2540e9802997bf80a099
YoKawaiiK <danil.legioner@gmail.com>
2020-12-23 16:55:15 +0700

    Изменение DetailActivity для корректного получения данных и заполнения ими экрана

--- app/src/main/java/com/udacity/Lab_4_part_2/DetailActivity.java
+++ app/src/main/java/com/udacity/Lab_4_part_2/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,18 +16,32 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    // Переменные для элементов
+    private ImageView ingredientsIv;
+    private TextView mPlaceOfOriginTv;
+    private TextView mDescriptionTv;
+    private TextView mIngredientsTv;
+    private TextView mAlsoKnownAsTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        // Получение по id элементов
+        ingredientsIv = findViewById(R.id.image_iv);
+        mPlaceOfOriginTv = findViewById(R.id.origin_tv);
+        mDescriptionTv = findViewById(R.id.description_tv);
+        mIngredientsTv = findViewById(R.id.ingredients_tv);
+        mAlsoKnownAsTv = findViewById(R.id.also_known_tv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
+        // Параметры, полученные при нажатии на элемент с списке
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
@@ -34,29 +49,52 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
+        // Получение массива sandwiches из строк, где каждая строка
+        // соответствует отдельному сэндвичу
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+
+        // Получение одного сэндвича по его позиции
         String json = sandwiches[position];
+
+        // Полученный sandwich десеаризируем и превращаем в объект
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
+        // Заполняем контентом
+        populateUI(sandwich);
     }
 
+    // Применяется, если сэндвич не найден
     private void closeOnError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    // Заполнение контентом
+    private void populateUI(Sandwich sandwich) {
+        // Название в header
+        setTitle(sandwich.getMainName());
+        Picasso.with(this)
+                .load(sandwich.getImage()).into(ingredientsIv);
 
+        mPlaceOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+
+        mDescriptionTv.setText(sandwich.getDescription());
+
+        // Преобразование массива в строку, где элементы массива разделены запятой
+        String formatTextIngredients = android.text.TextUtils.join(", ",
+                sandwich.getIngredients());
+        mIngredientsTv.setText(formatTextIngredients);
+
+        // Преобразование массива в строку, где элементы массива разделены запятой
+        String formatTextAlsoKnownAs = android.text.TextUtils.join(", ",
+                sandwich.getAlsoKnownAs());
+
+        mAlsoKnownAsTv.setText(formatTextAlsoKnownAs);
     }
 }
